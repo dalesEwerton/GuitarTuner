e965ae79320ebc03a75d8a917ef87604d14da8c2
thiagocm1 <30806616+thiagocm1@users.noreply.github.com>
2017-11-16 23:49:13 -0200

    Mouse Impl, Coordinates

--- src/GuitarTuner.cpp
+++ src/GuitarTuner.cpp
@@ -9,7 +9,7 @@
 void playSong(char song) {
 
     if (install_sound(DIGI_AUTODETECT, MIDI_AUTODETECT, NULL) != 0) {
-       allegro_message("Erro na inicialização do som\n%s\n", allegro_error);
+       allegro_message("Erro na inicializaÃ§Ã£o do som\n%s\n", allegro_error);
     }
 
     if(song == 'e') {
@@ -38,119 +38,116 @@ void playSong(char song) {
 
 }
 
-void play_app() {
-
+void play_app(){
         //Inicializando o buffer
         set_color_depth(32);
         set_gfx_mode(GFX_AUTODETECT_WINDOWED, WEIGHT, HEIGHT, 0, 0);
         BITMAP *buffer = create_bitmap(WEIGHT, HEIGHT);
 
         //Carregando as imagens
-        BITMAP *bg = load_bitmap("background.bmp",NULL);
+        BITMAP *background = load_bitmap("background.bmp",NULL);
         BITMAP *cursor = load_bitmap("cursor.BMP", NULL);
 
-        //Repetição da rotina de afinação
+        //RepetiÃ§Ã£o da rotina de afinaÃ§Ã£o
         while(!key[KEY_ESC]){
-
-            if( key[KEY_A])
+        /*AfinaÃ§Ã£o das cordas via teclado*/
+            if( key[KEY_S])
                 playSong('e');
 
-            if( key[KEY_S] )
+            if( key[KEY_B] )
                 playSong('B');
+
+            if( key[KEY_G] )
+                playSong('G');
 
             if( key[KEY_D] )
-                playSong('G');
-
-            if( key[KEY_H] )
                 playSong('D');
 
-            if( key[KEY_J] )
+            if( key[KEY_A] )
                 playSong('A');
 
-            if( key[KEY_K] )
+            if( key[KEY_E] )
                 playSong('E');
 
-
-        if(mouse_x >= 0 && mouse_x < 100
-           && mouse_y >= 0 && mouse_y < 100) {
-            rectfill(buffer, 0, 100, 0, 100, makecol(255,255,0));
-            rectfill(buffer,0,100,0,100,makecol(255,200,200));
-
+        /*Coordenadas x e y, definidas pelos limites
+        afinaÃ§ao via mouse*/
+        if(mouse_x >= 170 && mouse_x < 250
+           && mouse_y >= 50 && mouse_y < 130) {
             if(mouse_b == 1 ) {
-                playSong('e');
-            }
-
-            if(mouse_b == 2) {
-              textout_ex(buffer, font, "Nota ' e ' a ser pressionada!", 50, 50, makecol(255,255,255), -1);
-            }
-
-        }else if(mouse_x >= 100 && mouse_x < 200 && mouse_y >= 100 && mouse_y < 200){
-            if(mouse_b == 1 ){
-                playSong('B');
-            }
-            else if(mouse_b == 2){
-                textout_ex(buffer, font, "Nota ' B ' a ser pressionada!", 150, 150, makecol(255,255,255), -1);
-            }
-
-        }else if(mouse_x >= 200 && mouse_x < 300 && mouse_y >= 200 && mouse_y < 300){
-
-            if(mouse_b == 1){
-                playSong('G');
-            }
-            else if(mouse_b == 2){
-              textout_ex(buffer, font, "Nota ' G ' a ser pressionada!", 250, 250, makecol(255,255,255), -1);
-            }
-        } else if(mouse_x >= 300 && mouse_x < 400 && mouse_y >= 300 && mouse_y <= 400){
+             playSong('e');
+          }
+            else if(mouse_b == 2) {
+              textout_ex(buffer, font, "Nota ' e ' a ser pressionada!", 170, 30, makecol(0,0,255), -1);
+          }
+        }
+        else if(mouse_x >= 280 && mouse_x < 360 && mouse_y >= 70 && mouse_y < 130){
+                         if(mouse_b == 1 ){
+                            playSong('A');
+                         }
+                        else if(mouse_b == 2){
+                            textout_ex(buffer, font, "Nota ' A ' a ser pressionada!", 170, 30, makecol(0,0,255), -1);
+                }
+        }
+        else if(mouse_x >= 380 && mouse_x < 460 && mouse_y >= 70 && mouse_y < 130){
             if(mouse_b == 1){
                 playSong('D');
             }
             else if(mouse_b == 2){
-                 textout_ex(buffer, font, "Nota ' D ' a ser pressionada!", 350, 350, makecol(255,255,255), -1);
-            }
-        } else if(mouse_x >= 400 && mouse_x < 500 && mouse_y >= 100 && mouse_y < 200){
-            if(mouse_b == 1){
-                playSong('A');
-            }
-            else if(mouse_b == 2){
-                  textout_ex(buffer, font, "Nota ' A ' a ser pressionada!", 450, 150, makecol(255,255,255), -1);
-            }
-        } else if(mouse_x >= 400 && mouse_x < 500 && mouse_y >= 200 && mouse_y < 300){
-            if(mouse_b == 1){
-                playSong('E');
-            }
-            else if(mouse_b == 2){
-                 textout_ex(buffer, font, "Nota ' E ' a ser pressionada!", 450, 250, makecol(255,255,255), -1);
+              textout_ex(buffer, font, "Nota ' D ' a ser pressionada!", 170, 30, makecol(0,0,255), -1);
             }
         }
 
-        //LOCALIZACAO DAS NOTAS
-        textout_ex(buffer, font, "e", 50, 50, makecol(255,0,0), -1);
-        textout_centre_ex(buffer,font,"B", 150, 150, makecol(255,0,0), -1);
-        textout_centre_ex(buffer,font,"G", 250, 250, makecol(255,0,0), -1);
+        else if(mouse_x >= 170 && mouse_x < 250 && mouse_y >= 280 && mouse_y <= 370){
+            if(mouse_b == 1){
+                playSong('E');
+            }
+            else if(mouse_b == 2){
+                 textout_ex(buffer, font, "Nota ' E ' a ser pressionada!", 170, 30, makecol(0,0,255), -1);
+            }
+        }
 
-        textout_centre_ex(buffer,font,"D", 350, 350, makecol(255,0,0), -1);
-        textout_ex(buffer, font, "A", 450, 150, makecol(255,0,0), -1);
-        textout_ex(buffer, font, "E", 450, 250, makecol(255,0,0), -1);
+        else if(mouse_x >= 290 && mouse_x < 350 && mouse_y >= 300 && mouse_y < 370){
+            if(mouse_b == 1){
+                playSong('B');
+            }
+            else if(mouse_b == 2){
+                  textout_ex(buffer, font, "Nota ' B ' a ser pressionada!", 170, 30, makecol(0,0,255), -1);
+            }
+        }
 
-        //DRAW
-        draw_sprite(buffer, bg, 0,0);
+        else if(mouse_x >= 390 && mouse_x < 450 && mouse_y >= 300 && mouse_y < 370){
+            if(mouse_b == 1){
+                playSong('G');
+            }
+            else if(mouse_b == 2){
+                 textout_ex(buffer, font, "Nota ' G ' a ser pressionada!", 170, 30, makecol(0,0,255), -1);
+            }
+        }
+
         draw_sprite(buffer, cursor, mouse_x, mouse_y);
         draw_sprite(screen, buffer, 0, 0);
 
+        /*limpa o buffer para o prÃ³ximo loop*/
         clear(buffer);
+        //LIMPA O BUFFER E "RECOLORE" A TELA
+        rectfill(buffer, 2, 2, WEIGHT-4, HEIGHT-4, 0xC0C0C0);
+
+        rectfill(buffer, 30, 30, WEIGHT-30, HEIGHT-30,  0x008080);
+
+        blit(background,buffer,0,0,0,0,WEIGHT,HEIGHT);
+
     }
-
-
-
-    blit(buffer,screen,0,0,0,0,WEIGHT,HEIGHT);
-    destroy_bitmap(cursor);
-    destroy_bitmap(buffer);
-    destroy_bitmap(bg);
+    /*liberar memoria */
+        destroy_bitmap(cursor);
+        destroy_bitmap(buffer);
+        destroy_bitmap(background);
 }
+
 
 int main()
 {
     allegro_init();
+    set_window_title("Guitar Tuner");
     install_mouse();
     install_timer();
 	install_keyboard();
